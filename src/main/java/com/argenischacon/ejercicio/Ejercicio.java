f0067d6e5028c1caef0423bdfe75cd97532d1e6a
Argenis <argenis.18.alex@gmail.com>
2024-09-18 17:35:43 -0400

    Estado inicial del proyecto

---
+++ src/main/java/com/argenischacon/ejercicio/Ejercicio.java
@@ -0,0 +1,11 @@
+package com.argenischacon.ejercicio;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        //Agregando comentario
+        Persona perso = new Persona();//Instaciamos la clase persona
+        perso.pedirDatos();//Llamamos al metodo pedirDatos del objeto perso
+        System.out.println(perso.toString());//Mostramosel metodo toString (sobreescrito)
+    }
+}
