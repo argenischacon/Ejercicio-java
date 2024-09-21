b5142a5269c9e89a933cf9d96831d0641b94e7c7
Argenis <argenis.18.alex@gmail.com>
2024-09-21 18:20:54 -0400

    Factorial de un numero

--- src/main/java/com/argenischacon/ejercicio/Ejercicio.java
+++ src/main/java/com/argenischacon/ejercicio/Ejercicio.java
@@ -1,9 +1,37 @@
 package com.argenischacon.ejercicio;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio {
 
     public static void main(String[] args) {
-        
-     
+        int numero = 0;
+        boolean isValidNumber = false;
+        do {
+            try {
+                numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el numero del factorial: "));
+                //Llamar al metodo recursivo para calcular el factorial
+                JOptionPane.showMessageDialog(null, "El factorial del numero " + numero + " es: " + factorial(numero));
+                isValidNumber = true;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ingrese un numero entero valido");
+            }
+        } while (!isValidNumber);
+
     }
+
+    private static int factorial(int numero) {
+
+        if (numero > 0) {
+
+            return numero * factorial(numero - 1);
+
+        } else if (numero == 0) {//condicion de parada
+
+            return 1;
+        }
+
+        return numero;
+    }
+
 }
