86af9931cba2749a49f2d725ddade8246c6a5cec
Argenis <argenis.18.alex@gmail.com>
2024-09-21 17:43:18 -0400

    Limpiar proyecto

--- src/main/java/com/argenischacon/ejercicio/Ejercicio.java
+++ src/main/java/com/argenischacon/ejercicio/Ejercicio.java
@@ -4,8 +4,6 @@ public class Ejercicio {
 
     public static void main(String[] args) {
         
-        Persona perso = new Persona();//Instaciamos la clase persona
-        perso.pedirDatos();//Llamamos al metodo pedirDatos del objeto perso
-        System.out.println(perso.toString());//Mostramosel metodo toString (sobreescrito)
+     
     }
 }
