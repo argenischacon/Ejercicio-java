760493050e5eb11a3e4cc2955b7cb8cacd5488a4
Argenis <argenis.18.alex@gmail.com>
2024-09-27 23:30:26 -0400

    Ejercicio Generics (Diccionario Bidireccional)

--- src/main/java/com/argenischacon/ejercicio/Ejercicio.java
+++ src/main/java/com/argenischacon/ejercicio/Ejercicio.java
@@ -1,29 +1,21 @@
 package com.argenischacon.ejercicio;
 
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
-import javax.swing.JOptionPane;
+
 
 public class Ejercicio {
 
     public static void main(String[] args) {
     
-       GenericQueue<String> colaGenerica = new GenericQueue<>();
-       
-       colaGenerica.enqueue("Hola");
-       colaGenerica.enqueue("Mundo");
-       colaGenerica.enqueue(":D");
-        try {
-        System.out.println(colaGenerica.dequeue());
-        } catch (NoSuchElementException e) {
-            System.out.println("La pila esta vacia");
-        }
-        
-        System.out.println(colaGenerica.getElementos().toString());
-       
-        System.out.println(colaGenerica.isEmpty());
-        
-        System.out.println(colaGenerica.size());
+        BiDictionary<Integer, String> obj = new BiDictionary<>();
+        obj.put(1, "Hola");
+        obj.put(2, "mundo");
+        obj.put(3, "mundo");
+        obj.removeByValue("Hola");
+        System.out.println("Contiene el valor?: "+obj.containsValue("mundo"));
+        System.out.println("Tamaño: "+obj.size());
+        System.out.println(obj.get_key_value().toString());
+        System.out.println(obj.get_value_key().toString());
+                
         
     }
 }
